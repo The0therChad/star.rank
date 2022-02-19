5de0542f8524523365d54a924eeed85da09707cd
Monica <77224723+Energix11@users.noreply.github.com>
2022-02-19 07:22:46 -0800

    Update rank_species.R
    doc update

--- R/rank_species.R
+++ R/rank_species.R
@@ -1,58 +1,65 @@
 #' Rank Species by specified metric
 #'
-#' @importFrom purrr compact
-#' @importFrom jsonlite fromJSON
-#' @importFrom httr GET
+#' Returns a plot showing the top 15 Star Wars species
+#' sorted by a specified metric with starship name on the y-axis,
+#' and the ranking metric on the x-axis.
 #'
-#' @return
-#' @export
+#' @param interested Metric to rank species on. ("average_height", "average_lifespan")
 #'
-#' @importFrom purrr compact
-#' @importFrom jsonlite fromJSON
-#' @importFrom httr GET
 #' @import ggplot2
-#' @import dplyr
+#' @importFrom utils head
 #'
-#' @return
+#' @return A plot with species name on the y-axis and the ranking metric on the x-axis.
 #' @export
 #'
-#' @examples
+#' @examples \dontrun{rank_species(interested = "average_height"),
+#' rank_species(interested = "average_lifespan")} 
 
-rank_species <- function(format = NULL, interested) {
+# Get information
+rank_species <- function(interested = NULL) {
   url <- "https://swapi.dev/api/species/"
-  args <- list(format = format)
+  #check for internet
+  check_internet()
   # Call the API
-  res <- GET(url, query = compact(args))
+  res <- httr::GET(url)
+  # Check the result
+  check_status(res)
   # Store content of response
   cont <- httr::content(res, as = "text", encoding = "UTF-8")
   # Return content as a dataframe
-  resHead <- fromJSON(cont)
+  resHead <- jsonlite::fromJSON(cont)
   resDF <- resHead$results
   # Get each page of data and append to first page
   while (!is.null(resHead$`next`)) {
-    res <- GET(resHead$`next`, query = compact(args))
+    res <- httr::GET(resHead$`next`)
     cont <- httr::content(res, as = "text", encoding = "UTF-8")
-    resHead <- fromJSON(cont)
+    resHead <- jsonlite::fromJSON(cont)
     resDF <- rbind(resDF, resHead$results)
   }
+  
+  # Tidy dataframe 
+  # Trim to only 'name' and numeric columns
+  resDF <- 
+    resDF[c(
+      'name', 
+      'average_height', 
+      'average_lifespan'
+    )]
+  # Strip commas, change numeric values to numbers
+  resDF[, c(2:3)] <- suppressWarnings(sapply(resDF[, c(2:3)], function(x) as.numeric9gsub(",", "", x))))
+  # Sort by specified metic and use only top 15 values
+  resDF <- resDF %>%
+    dplyr::arrange(-!!sym(interested)) %>%
+    head(15)
 
-  #trim and sort dataframe for plotting
-  resDF <- resDF[c('name', 'average_height', 'average_lifespan')]
-  resDF[, c(2:3)] <- sapply(resDF[, c(2:3)], as.numeric)
-  resDF <- resDF[complete.cases(resDF), ] %>%
-    arrange(-!!sym(interested))
-  resDF <- head(resDF, 20)
-  resDF
 
-  #plot output for average_lifespan or average_height
-
-  speciesplot <- ggplot(data=resDF,
-                        aes(x=reorder(name, -!!sym(interested)),
-                            y=!!sym(interested))) +
+  # Plot results
+  speciesPlot <- 
+    ggplot(resDF, aes(x=stats::reorder(name, -!!sym(interested)), y=!!sym(interested))) +
     geom_bar(stat="identity") +
     coord_flip() +
     xlab("Species")
-  speciesplot
+  speciesPlot
 
 }
 
