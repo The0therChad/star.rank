ae43697779959d6b80396f24cf683d8fc4345520
Monica <77224723+Energix11@users.noreply.github.com>
2022-02-20 00:05:37 -0800

    Update test_rank_starships.R

--- tests/testthat/test_rank_starships.R
+++ tests/testthat/test_rank_starships.R
@@ -1,5 +1,7 @@
 library("star.rank")
 
+
+# Test rank_starships function (args = (interested, n=optional))
 test_that("" , {
   expect_error(rank_starships(interested = "test"))
   expect_error(rank_starships(), "'interested' argument must be character string")
