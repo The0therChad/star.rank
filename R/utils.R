68918b92df4e9bb6adc17281841b77654ac345f2
Monica <77224723+Energix11@users.noreply.github.com>
2022-02-19 23:59:02 -0800

    MonicaFinalEdits

--- R/utils.R
+++ R/utils.R
@@ -1,16 +1,19 @@
 #' @importFrom attempt stop_if_not
 #' @importFrom curl has_internet
+# Check internet connection
 check_internet <- function(){
   stop_if_not(.x = has_internet(), msg = "Please check your internet connection")
 }
 
 #' @importFrom httr status_code
+# Check API return status
 check_status <- function(res){
   stop_if_not(.x = status_code(res),
               .p = ~ .x == 200,
               msg = "The API returned an error")
 }
 
+# Check "in" function to confirm "interested" is in data
 '%!in%' <- function(x,y)!('%in%'(x,y))
 
 utils::globalVariables(c("name", "title", "%>%"))
